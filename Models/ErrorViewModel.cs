8b134ac6134353b51988696ca3fb1df81d2dbee2
gGybzz2297@gmail.com <gGybzz2297@gmail.com>
2020-02-06 15:43:49 +0800

    initial create

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AsinTransientAndApartment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
